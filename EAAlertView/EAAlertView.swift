5ce7793b6f3ccb10562145c46915a90b9286db5f
Egzon Arifi <egzon.arifi@jaywalker-digital.ch>
2018-08-10 19:38:38 +0200

    alert class test

---
+++ EAAlertView/EAAlertView.swift
@@ -0,0 +1,17 @@
+//
+//  EAAlertView.swift
+//  EAAlertView
+//
+//  Created by Egzon Arifi on 8/10/18.
+//  Copyright © 2018 Overjump. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+open class EAAlertView: NSObject {
+    
+    init(test: String) {
+        print(test)
+    }
+}
