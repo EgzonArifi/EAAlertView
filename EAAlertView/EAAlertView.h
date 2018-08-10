0668f5a8ac6cb10e3a3b1626565258b0d2138f64
Egzon Arifi <egzon.arifi@jaywalker-digital.ch>
2018-08-10 18:55:25 +0200

    added initial project setup

---
+++ EAAlertView/EAAlertView.h
@@ -0,0 +1,19 @@
+//
+//  EAAlertView.h
+//  EAAlertView
+//
+//  Created by Egzon Arifi on 8/10/18.
+//  Copyright © 2018 Overjump. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EAAlertView.
+FOUNDATION_EXPORT double EAAlertViewVersionNumber;
+
+//! Project version string for EAAlertView.
+FOUNDATION_EXPORT const unsigned char EAAlertViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EAAlertView/PublicHeader.h>
+
+
